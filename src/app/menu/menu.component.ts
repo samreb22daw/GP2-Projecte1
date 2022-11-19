4bcf776665c7553704c89e7b2dba62dba6103134
xavara22daw <xavara22daw@proton.me>
2022-11-19 16:06:01 +0100

    3 animacions agregades

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  animations: [
+    trigger("animacioMenu", [
+      state('hidden', style({
+        transform: 'translateX(0%)', opacity:0})
+      ), state('shown', style({
+        transform: 'translateX(100%)', opacity: 1})
+      ), transition('hidden => shown', [
+        animate('0.5s')
+      ]),
+    ])
+  ]
 })
 export class MenuComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  state = 'hidden';
+
+  ngAfterViewInit() {
+    setTimeout( () => {
+      this.state = 'shown';
+    }, 200);
   }
 
   isActive = false;
