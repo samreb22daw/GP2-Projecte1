5cdd3ffd44a312141030d1dc871c3201885813b1
guibad22daw <guibad22daw@proton.me>
2022-11-21 02:42:52 +0100

    Afegida animació formulari

--- src/app/formulari/formulari.component.ts
+++ src/app/formulari/formulari.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, animate, transition, style } from '@angular/animations';
 
 @Component({
   selector: 'app-formulari',
   templateUrl: './formulari.component.html',
-  styleUrls: ['./formulari.component.css']
+  styleUrls: ['./formulari.component.css'],
+  animations: [
+    trigger("animacioForm", [
+      state('hidden', style({
+        transform: 'scale(30%)', opacity:0})
+      ), state('shown', style({
+        transform: 'scale(100%)', opacity: 1})
+      ), transition('hidden => shown', [
+        animate('0.4s')
+      ]),
+    ])
+  ]  
 })
 export class FormulariComponent implements OnInit {
+  state = 'hidden';
 
   constructor() { }
+
+  ngAfterViewInit() {
+    setTimeout( () => {
+      this.state = 'shown';
+    }, 0);    
+  }
 
   ngOnInit(): void {
   }
